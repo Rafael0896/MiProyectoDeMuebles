4dfe8e00dd98a3b16c26b90105906979659df9df
Rafael0896 <realvarez54@misena.edu.co>
2025-07-02 22:34:19 -0500

    primer commit

---
+++ src/main/java/edu/sena/creamuebles/CreamueblesApplication.java
@@ -0,0 +1,13 @@
+package edu.sena.creamuebles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreamueblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreamueblesApplication.class, args);
+	}
+
+}
